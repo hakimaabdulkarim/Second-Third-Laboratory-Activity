1adc3498630e7630b0cebb393d6911cbce41a762
hakimaabdulkarim <144906884+hakimaabdulkarim@users.noreply.github.com>
2026-05-31 00:21:36 +0800

    Create votes table with relevant fields

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,11 @@
+CREATE TABLE votes (
+    doc_id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    poll_id TEXT NOT NULL,
+    choice TEXT NOT NULL,
+    timestamp DOUBLE PRECISION,
+    edge_id TEXT,
+    processed_at DOUBLE PRECISION
+);
 
+ALTER TABLE votes DISABLE ROW LEVEL SECURITY;
